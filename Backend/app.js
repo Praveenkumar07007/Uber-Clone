32fed7663bf5eb398832a6c4db35a2fa5b79f371
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-01-20 23:33:40 +0530

    add captain registration functionality and related routes, model, and service

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 const express = require('express');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 const cors = require('cors');
 const app = express();
 app.use(cors());
@@ -12,5 +13,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
 
 module.exports = app;
