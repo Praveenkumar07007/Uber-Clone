b18f92804f9cb7374491e8fded4a6e136323e25d
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-01-20 22:59:08 +0530

    implement user login functionality and improve registration process

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
+const userController = require('../controllers/user.controller');
 const {body} = require('express-validator')
 
 router.post('/register', [
@@ -8,7 +8,14 @@ router.post('/register', [
   body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters long'),
   body('fullname.firstname').isLength({min: 2}).withMessage('First name must be at least 2 characters long'),
   body('fullname.lastname').isLength({min: 2}).withMessage('Last name must be at least 2 characters long')
-],)
+],userController.registerUser)
+
+router.post('/login',[
+  body('email').isEmail().withMessage('Invalid Email'),
+  body('password').isLength({min:6}).withMessage('password must be at least 6 characters')
+],
+userController.loginUser
+)
 
 
 module.exports = router;
--- Backend/services/user.service.js
+++ Backend/services/user.service.js
@@ -1,16 +1,21 @@
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
+const { validationResult } = require('express-validator');
 
-
-module.exports.createUser = async({
-  firstname , lastname , email , password
-})=>{
-  if(!firstname || !email || !password){
-    throw new Error('All fields are required');
+module.exports.registerUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
   }
-  const user = await userModel.create({
-    fullname:{
-      firstname , lastname
-    }, email , password
-  })
-  return user;
+  const { fullname, email, password } = req.body;
+  const hashedPassword = await userModel.hashPassword(password);
+  const user = await userService.createUser({
+      firstname: fullname.firstname,
+      lastname: fullname.lastname,
+      email,
+      password: hashedPassword
+  });
+  const token = user.generateAuthToken();
+  res.status(201).json({ token, user });
 }
+
